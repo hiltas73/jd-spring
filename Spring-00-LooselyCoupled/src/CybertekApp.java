03c4e888d01570211ac225cec5d28cbd902e8c76
hiltas73 <h_iltas@yahoo.com>
2020-10-21 16:14:24 +0300

    LooselyCoupled_Oct21_2

--- Spring-00-LooselyCoupled/src/CybertekApp.java
+++ Spring-00-LooselyCoupled/src/CybertekApp.java
@@ -1,5 +1,6 @@
 import service.FullTimeMentor;
 import service.MentorAccount;
+import service.PartTimeMentor;
 
 public class CybertekApp {
     public static void main(String[] args) {
@@ -7,7 +8,9 @@ public class CybertekApp {
         FullTimeMentor fullTime = new FullTimeMentor();
         //PartTimeMentor partTime = new PartTimeMentor();
 
-        MentorAccount mentor = new MentorAccount(fullTime);
+        MentorAccount mentor = new MentorAccount(fullTime); //whatever we pass in here (partTime
+                                    // or fullTime) we create a new account so easy to make changes
+                                    // compared to TightlyCoupled
 
         mentor.manageAccount();
 
